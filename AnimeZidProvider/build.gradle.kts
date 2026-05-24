433b40a5d243a6017ee644529b26994b3d42927a
guesttn <guesttn963@gmail.com>
2026-05-24 05:53:45 +0100

    Create build.gradle.kts

---
+++ AnimeZidProvider/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    language = "ar"
+    description = "موقع انمي زد للأنمي والكرتون المدبلج والمترجم"
+    authors = listOf("YourName")
+    tvTypes = listOf("Anime", "AnimeMovie", "Movie", "TvSeries")
+    iconUrl = "https://animezid.cam/uploads/custom-logo.png"
+}
