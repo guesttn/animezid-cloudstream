f2a1077424b5d5ab98c138eb7860d4dd46cc3335
guesttn <guesttn963@gmail.com>
2026-05-24 06:05:14 +0100

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    }
+}
+
+tasks.register("make") {
+    dependsOn(gradle.includedBuilds.map { it.task(":make") })
+}
